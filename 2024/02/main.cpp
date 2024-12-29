052a65af48a8f4bcec5924e5616c32abbf87822a
Julian Cooper <julian.e.cooper2@gmail.com>
2024-12-29 02:20:15 +0100

    cpp solution for 2.1

--- 2024/02/main.cpp
+++ 2024/02/main.cpp
@@ -40,34 +40,41 @@ bool isMonotonicDecreasing(const std::vector<int>& vec) {
     return std::adjacent_find(vec.begin(), vec.end(), std::less<int>()) == vec.end();
 }
 
-int levelsCheck(const std::vector<int>& vec) {
-  std::vector<bool> checks;
-  for (size_t k = 0; k < vec.size() - 1; ++k) {
-    int delta = abs(vec[k] - vec[k+1]);
-    if (delta >= 1 && delta <= 3) {
-      checks.push_back(true);
-    }
-    else {
-      checks.push_back(false);
+bool isStrictlyMonotonic(const std::vector<int>& vec) {
+  bool increasing = true;
+  bool decreasing = true;
+
+  for (size_t i = 1; i < vec.size(); ++i) {
+    if (vec[i] > vec[i - 1]) {
+      decreasing = false;
+    } else if (vec[i] - vec[i - 1]) {
+      increasing = false;
+    } else {
+      return false;
     }
   }
-  int sum = std::accumulate(std::begin(checks), std::end(checks), 0); 
-  return sum;
+  
+  return increasing || decreasing;
 }
 
-int numSafeReports(std::vector<std::vector<int>> data) {
+bool levelsCheck(const std::vector<int>& vec) {
+    for (size_t k = 0; k < vec.size() - 1; ++k) {
+        int delta = std::abs(vec[k] - vec[k + 1]);
+        if (delta < 1 || delta > 3) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int numSafeReports(const std::vector<std::vector<int>>& data) {
   int numSafe = 0;
-  for (size_t i = 0; i < data.size(); ++i) {
-    std::vector<int> diffs;
-    for (size_t j = 0; j < (data[i].size()-1); ++j) {
-      diffs.push_back(data[i][j] - data[i][j+1]);
-    }
-    // check montonicity
-    if (isMonotonicIncreasing(diffs)) {
-      numSafe += levelsCheck(diffs);
-    }
-    else if (isMonotonicDecreasing(diffs)) {
-      numSafe += levelsCheck(diffs);
+  
+  for (const auto& report : data) {
+    
+    // check montonicity and level constraints
+    if (isStrictlyMonotonic(report) && levelsCheck(report)) {
+      numSafe++;
     }
   }
   return numSafe;
@@ -78,6 +85,7 @@ int main(int argc, char *argv[]) {
     // no arguments, print useage message
     std::cout << "Usage: " <<std::endl;
     std::cout << "$ ./main <input data file>" << std::endl;
+    return 1; // exit with error code
   }
 
   // read data file
@@ -85,13 +93,13 @@ int main(int argc, char *argv[]) {
   std::vector<std::vector<int>> data = readFile(fileName);
 
   // print first two rows (reports) of data
-  //for (size_t i = 0; i < 2; ++i) {
-  //  std::cout << "Report " << i + 1 << ": ";
-  //  for (int num : data[i]) {
-  //    std::cout << num << " ";
-  //  }
-  //  std::cout << std::endl;
-  //}
+  for (size_t i = 0; i < 2; ++i) {
+    std::cout << "Report " << i + 1 << ": ";
+    for (int num : data[i]) {
+      std::cout << num << " ";
+    }
+    std::cout << std::endl;
+  }
 
   // q1: check report safety
   int num_safe = numSafeReports(data);
