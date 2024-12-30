b8b9db8cc7c26184dcab0bc2aa10a553c4632890
Julian Cooper <julian.e.cooper2@gmail.com>
2024-12-30 12:44:39 +0100

    python solution for 3.2

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -14,15 +14,47 @@ if __name__ == '__main__':
             data.append(line)
 
     # match pattern
-    pattern = r"mul\((\d+),(\d+)\)"    
+    pattern = r"mul\((\d+),(\d+)\)(.*?)(?=mul\(|$)"
+    subpattern = r"(do\(\)|don't\(\))"
+
+    # extract mult(#,#) integer pairs      
     matches_list = list()
     for text_string in data:
-        matches = [(int(x), int(y)) for x, y in re.findall(pattern, text_string)]
+        matches = re.finditer(pattern, text_string)
         matches_list.append(matches)
-    flattened_matches_list = list(chain.from_iterable(matches_list))
 
+    # combine with do() and don't() instructions
+    flattened_matches_list = list()
+    for match in list(chain.from_iterable(matches_list)):
+        first_mult = (int(match.group(1)), int(match.group(2)))
+        in_between = match.group(3)
+        found_patterns = re.findall(subpattern, in_between)
+        flattened_matches_list.append((first_mult, found_patterns))
+    #print(flattened_matches_list)
+   
     # q1: compute partial result
     result = 0
-    for mult in flattened_matches_list:
+    for match in flattened_matches_list:
+        mult = match[0]  # multiply statement
         result += mult[0] * mult[1]
-    print(f"Partial result: {result}")
+    
+    print(f"q1 result: {result}")
+
+    # q2: add switch commands
+    result = 0
+    switch = True
+    for match in flattened_matches_list:
+        mult, cmds = match
+        
+        if switch:
+            result += mult[0] * mult[1]
+
+        for cmd in cmds:
+            if cmd == "do()":
+                switch = True
+            elif cmd == "don't()":
+                switch = False
+            else:
+                ValueError(f"Command {cmd} not recognized")
+    
+    print(f"q2 result: {result}")
