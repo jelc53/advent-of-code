0e8240f361f51e6d36e95176969a707c9765c37f
Julian Cooper <julian.e.cooper2@gmail.com>
2025-01-13 05:30:24 +0100

    wip rust solution for 5.1

--- 2024/05/ex1.rs
+++ 2024/05/ex1.rs
@@ -62,6 +62,58 @@ fn load_updates_as_vec_of_vecs(file_path: &str) -> io::Result<Vec<Vec<i32>>> {
     }
 }
 
+fn apply_rule(update: &Vec<i32>, a: i32, b: i32) -> bool {
+    // extract positions
+    let pos_a = update.iter().position(|&x| x == a);
+    let pos_b = update.iter().position(|&x| x == b);
+
+    // edge case: a, b equal
+    if a == b {
+        return true;
+    }
+
+    // edge case: a. b not both in rule
+    if pos_a.is_none() && pos_b.is_none() {
+        return true;
+    }
+
+    // check if a comes before b
+    match (pos_a, pos_b) {
+        (Some(index_a), Some(index_b)) => index_a < index_b,
+        _ => false,
+    }
+}
+
+fn check_if_valid(rules: Vec<(i32, i32)>, update: Vec<i32>) -> bool {
+    for (a, b) in rules {
+        if !apply_rule(&update, a, b) {
+            return false;
+        }
+    }
+    true // all rules pass
+}
+
+fn middle_element(vec: &Vec<i32>) -> Option<i32> {
+    if vec.len() % 2 == 1 {
+        // calculate the middle index for odd-length vec
+        let mid_index = vec.len() / 2;
+        Some(vec[mid_index])
+    } else {
+        None // return none if vector length not odd
+    }
+}
+
+fn get_valid_update_values(rules: Vec<(i32, i32)>, updates: Vec<Vec<i32>>) -> Vec<i32> {
+    let mut out_vec = Vec::new();
+    for update in updates {
+        if check_if_valid(rules, update) {
+            let value = middle_element(update);
+            out_vec.push(value);
+        }
+    }
+    out_vec
+}
+
 fn main() {
     // get command line args
     let args: Vec<String> = env::args().collect();
@@ -88,5 +140,6 @@ fn main() {
     };
 
     // check update validity
-    // let update_values = get_valid_update_values(rules, updates);
+    let update_values = get_valid_update_values(rules, updates);
+    println!("Result q1: {:?}", update_values.iter().sum());
 }
